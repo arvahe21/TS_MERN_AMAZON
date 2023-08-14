1a72ad8db55610cd5981f765dbec442385e9faf1
arvahe21 <vahear03@gmail.com>
2023-08-14 23:08:18 +0400

    Second Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +14,17 @@ function App() {
       </header>
 
       <main>
-
+        <ul>
+        {
+          sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img className='product-image' src={product.image} alt={product.name} />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))
+        }
+        </ul>
       </main>
 
       <footer>
